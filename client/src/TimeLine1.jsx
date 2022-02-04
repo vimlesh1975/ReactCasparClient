8da492a9372992544ef0b515cb68a1438136f8a4
vimlesh1975 <vimlesh1975@hotmail.com>
2022-02-04 17:42:50 +0530

    timeline progress

--- client/src/TimeLine1.jsx
+++ client/src/TimeLine1.jsx
@@ -1,69 +1,97 @@
 import React, { useState, useRef } from "react";
-import { useSelector } from 'react-redux'
+import { useEffect } from "react";
+import { useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+
 var interval;
-var left=0;
+var left = 0;
 const TimeLine1 = () => {
   const canvas = useSelector(state => state.canvasReducer.canvas);
+  const layers = useSelector(state => state.canvasReducer.canvas?.getObjects());
+  // var aaa=[layers.length];
+
   // const canvasList = useSelector(state => state.canvasListReducer.canvasList);
   // const [left, setLeft] = useState(0)
 
-  const [keyFrames,setkeyFrames]=useState([])
-const refCanvasItem0=useRef();
-  const aa = (val) => {
-    // setLeft(val)
-    left=val;
-    canvas?.item(0).set({left:val});
-    canvas?.requestRenderAll();
+  const [keyFrames, setkeyFrames] = useState({keyFrame0:[], keyFrame1:[]})
+  const refCanvasItem = useRef([]);
+  refCanvasItem.current = [];
 
-  }
- const  addKeyFrame=()=>{
-  setkeyFrames([...keyFrames, { left: left}])
- }
-  const playTimeline = () => {
-    clearInterval(interval)
-    canvas?.item(0).set({left:keyFrames[0].left});
-    canvas?.requestRenderAll();
-setTimeout(() => {
-  interval = setInterval(() => {
-    if (left < keyFrames[1].left) {
-       left=left+10;
-       canvas?.item(0).set({left:left});
-       canvas?.requestRenderAll();
-      }
-    else {
-      clearInterval(interval)
+  const addtoRefs = el => {
+    if (el && !refCanvasItem.current.includes(el)) {
+      refCanvasItem.current.push(el);
     }
-    refCanvasItem0.current.value=left;
-  }, 10);
-}, 1000);
-
-   
   }
-  const stopTimeline = () => {
+
+
+// useEffect(()=>{
+//  console.log(aaa);  
+// //  setkeyFrames([...layers])
+// },[aaa])
+
+  const aa = (e) => {
+    left = parseInt(e.target.value);
+    canvas?.item(e.target.getAttribute('key1')).set({ left: parseInt(e.target.value) });
+    // console.log((refCanvasItem.current[e.target.getAttribute('key1')]?.value));
+    canvas?.requestRenderAll();
+  }
+  const addkeyFrame = (i) => {
+    setkeyFrames({    ...keyFrames,keyFrame0:[...keyFrames.keyFrame0, {left:left}]         } )
+  }
+
+
+
+  const playTimeline = (i) => {
+    clearInterval(interval)
+    canvas?.item(i).set({ left: keyFrames[i].left });
+    canvas?.requestRenderAll();
+    setTimeout(() => {
+      interval = setInterval(() => {
+        if (left < keyFrames[1].left) {
+          left = left + 10;
+          canvas?.item(i).set({ left: left });
+          canvas?.requestRenderAll();
+        }
+        else {
+          clearInterval(interval)
+        }
+        refCanvasItem.current[i].value = left;
+      }, 10);
+    }, 1000);
+
+
+  }
+  const stopTimeline = (i) => {
     clearInterval(interval)
   }
 
   return (<div>
-   
-    Size<input ref={refCanvasItem0} style={{width:500}} onChange={e => aa(parseInt(e.target.value))} type="range" min={0} max={1024} step={1} defaultValue={0} />
-    <button onClick={playTimeline}>Play</button>
-    <button onClick={stopTimeline}>Stop</button>
+
+    {layers?.map((element, i) => {
+      return (<div key={uuidv4()} >
+        {element.type}<input ref={addtoRefs} key1={i} style={{ width: 500 }} onChange={e => aa(e)} type="range" min={0} max={1024} step={1} defaultValue={0} />
+        <div style={{ width: 500, display: 'flex', justifyContent: 'space-around' }}>
+          {keyFrames.keyFrame0.map((val) =>
+            <div> <button onClick={() => {
+              // aa(val.left)
+              // refCanvasItem[i].current.value = val.left;
+
+            }
+            }>{val.left}</button></div>
+          )}
+        </div>
+        <button onClick={()=>addkeyFrame(i)} >Add KeyFrame</button>
+
+        <button onClick={() => playTimeline(i)}>Play</button>
+        <button onClick={() => stopTimeline(i)}>Stop</button>
+      </div>)
+    })
+    }
+
     <div>
-    <div style={{ width:500,display:'flex', justifyContent:'space-around'}}>
-    {keyFrames.map((val)=>
-     <div> <button onClick={()=>{
-      aa(val.left)
-      refCanvasItem0.current.value=val.left;
-
-     }
-    }>{val.left}</button></div>
-    )}
-    </div>
-
 
     </div>
     <div>
-    <button onClick={addKeyFrame}>Add KeyFrame</button>
     </div>
   </div>)
 
