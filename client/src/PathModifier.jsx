1563a7423e879755c7a2a03a1df3a5c3b4fa26e2
vimlesh1975 <vimlesh1975@hotmail.com>
2022-02-19 19:55:58 +0530

    Removed runtime errors drom pathModifier

--- client/src/PathModifier.jsx
+++ client/src/PathModifier.jsx
@@ -1,49 +1,69 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 
-
+var bb;
 const PathModifier = () => {
     const canvas = useSelector(state => state.canvasReducer.canvas);
     const [path1, setPath1] = useState([]);
-    const [path2, setPath2] = useState([]);
 
     const showpaths = () => {
-        const aa = [...canvas?.getActiveObjects()[0]?.path];
-        setPath1([...aa]);
-        setPath2([...aa]);
+        if (canvas.getActiveObjects()[0]?.type === 'path') {
+            const aa1 = [...canvas?.getActiveObjects()[0]?.path];
+            bb = JSON.stringify(aa1);
+            setPath1([...aa1]);
+        }
     }
     const resetPaths = () => {
-        console.log(path2);
-        setPath1([...path2]);
+        if (canvas.getActiveObjects()[0]?.type === 'path') {
+            setPath1(JSON.parse(bb));
+            canvas.getActiveObjects()[0].set({ path: JSON.parse(bb) });
+            canvas?.requestRenderAll();
+        }
     }
+
     const resetValue = (i, ii) => {
-        console.log(path2);
-        setPath1([...path2]);
+        if (canvas.getActiveObjects()[0]?.type === 'path') {
+            const aa2 = [...path1];
+            aa2[i][ii] = JSON.parse(bb)[i][ii];
+            setPath1([...aa2]);
+            canvas.getActiveObjects()[0].set({ path: aa2 });
+            canvas?.requestRenderAll();
+        }
+    }
+    const resetValuePoint = i => {
+        if (canvas.getActiveObjects()[0]?.type === 'path') {
+            const aa2 = [...path1];
+            aa2[i] = JSON.parse(bb)[i];
+            setPath1([...aa2]);
+            canvas.getActiveObjects()[0].set({ path: aa2 });
+            canvas?.requestRenderAll();
+        }
     }
 
     const updatePath1 = (i, ii, e) => {
-        const aa = [...path1];
-        aa[i][ii] = parseInt(e.target.value);
-        setPath1([...aa]);
-        canvas.getActiveObjects()[0].set({ path: [...path1] });
-        canvas?.requestRenderAll();
-        // console.log(path1[i][ii]);
-        // console.log(path2[i][ii]);
+        if (canvas.getActiveObjects()[0]?.type === 'path') {
+            const aa2 = [...path1];
+            aa2[i][ii] = parseInt(e.target.value);
+            setPath1([...aa2]);
+            canvas.getActiveObjects()[0].set({ path: aa2 });
+            canvas?.requestRenderAll();
+        }
     }
 
     return (<div>
-        <button onClick={showpaths}>showpaths</button>  <button onClick={resetPaths}>Reset path</button>
-
+        <div style={{ paddingBottom: 10 }}>
+            <button onClick={showpaths}>showpaths</button>  <button onClick={resetPaths}>Reset path</button>
+        </div>
         <div style={{ maxHeight: 800, border: '1px solid grey', overflow: 'scroll' }}>
 
             {path1?.map((val, i) => {
                 return (<div key={i} style={{ maxWidth: 800, border: '1px solid grey', marginBottom: 10, paddingBottom: 10 }}>
-                    Point {i + 1}/{path1.length} {val.map((vv, ii) => {
+                    Point {i + 1}/{path1.length} <button onClick={() => resetValuePoint(i)} >Reset</button>
+                    {val.map((vv, ii) => {
                         return (<div key={ii} >
                             {(ii === 0) ? <><label style={{ width: 40 }} > {vv}</label></> : ''}
                             {(ii > 0) ? <><input style={{ width: 400 }} onChange={e => updatePath1(i, ii, e)} type="range" min={-1000} max={1000} step='1' value={vv} />
                                 <input style={{ width: 50 }} onChange={e => updatePath1(i, ii, e)} type="number" min={-1000} max={1000} step='1' value={vv.toFixed(0)} /> <button onClick={() => resetValue(i, ii)} >Reset</button>
-
                             </> : ''}
                         </div>)
                     })}
