9442ecbbe77996a78bb33861edac02635dee063e
vimlesh1975 <vimlesh1975@hotmail.com>
2022-07-11 10:25:07 +0530

    final check

--- client/src/TimeLine1.jsx
+++ client/src/TimeLine1.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Rnd } from 'react-rnd';
 import { endpoint } from './common';
