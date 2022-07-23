4621f22f2f37b5079183c62187998e3ce94bd470
vimlesh1975 <vimlesh1975@hotmail.com>
2022-07-23 17:06:47 +0530

    CSV Reader

--- client/src/CsvReader.jsx
+++ client/src/CsvReader.jsx
@@ -1,90 +1,122 @@
-import { useState } from 'react'
+// import { useState } from 'react'
+import { endpoint } from './common'
+import { useSelector, useDispatch } from 'react-redux'
+import { fabric } from "fabric";
 
-export default function CsvReader(){
-    const [csvFile, setCsvFile] = useState();
-    const [csvArray, setCsvArray] = useState([]);
-    // [{name: "", age: 0, rank: ""},{name: "", age: 0, rank: ""}]
+export default function CsvReader() {
+    const csvArray = ["ccgf0; ccgf1;ccgf2;ccgf3",
+        "booker12;9012;Rachel;Booker",
+        "grey07;2070;Laura;Grey",
+        "johnson81;4081;Craig;Johnson",
+        "jenkins46;9346;Mary;Jenkins",
+        "smith79;5079;Jamie;Smith"];
+    const currentscreenSize = localStorage.getItem('RCC_currentscreenSize');
+    const canvasList = useSelector(state => state.canvasListReducer.canvasList);
+    const canvas = useSelector(state => state.canvasReducer.canvas);
+    const dispatch = useDispatch();
 
-    const processCSV = (str, delim=',') => {
-        // const headers = str.slice(0,str.indexOf('\n')).split(delim);
-        // const rows = str.slice(str.indexOf('\n')+1).split('\n');
-        // console.log(rows);
-
-        var aa = str.split('\r\n');
-        aa.splice(-1);
-        var updatedcanvasList = [];
-        aa.forEach(element => {
-            var cc = element;
-            updatedcanvasList.push(cc)
-        });
-        setCsvArray(updatedcanvasList);
-        console.log(updatedcanvasList)
-        // const newArray = rows.map( row => {
-        //     const values = row.split(delim);
-        //     const eachObject = headers.reduce((obj, header, i) => {
-        //         obj[header] = values[i];
-        //         console.log(obj)
-        //         return obj;
-        //     }, {})
-        //     return eachObject;
-        // })
-
-        // setCsvArray(newArray)
-    }
-
-    const submit = () => {
-        const file = csvFile;
-        const reader = new FileReader();
-
-        reader.onload = function(e) {
-            const text = e.target.result;
-            console.log(text);
-            processCSV(text)
+    const recallPage = (layerNumber, pageName, data) => {
+        const index = canvasList.findIndex(val => val.pageName === pageName);
+        if (index !== -1) {
+            dispatch({ type: 'CHANGE_CURRENT_PAGE', payload: index })
+            const data1 = data;
+            canvas.loadFromJSON(canvasList[index].pageValue, () => {
+                data1.forEach(data2 => {
+                    canvas.getObjects().forEach((element) => {
+                        try {
+                            if (element.id === data2.key) {
+                                if (data2.type === 'text') {
+                                    const originalWidth = element.width;
+                                    element.set({ objectCaching: false, text: data2.value.toString() })
+                                    if (element.textLines.length > 1) {
+                                        do {
+                                            element.set({ width: element.width + 5 });
+                                        }
+                                        while (element.textLines.length > 1);
+                                        element.set({ scaleX: originalWidth / element.width });
+                                    }
+                                }
+                                else if (data2.type === 'image') {
+                                    var i = new Image();
+                                    i.onload = function () {
+                                        const originalWidth = (element.width) * (element.scaleX);
+                                        const originalHeight = (element.height) * (element.scaleY);
+                                        element.set({ objectCaching: false, scaleX: (originalWidth / i.width), scaleY: (originalHeight / i.height) })
+                                        if (element.type === 'image') {
+                                            element.setSrc(data2.value)
+                                        }
+                                        else if (element.type === 'rect') {
+                                            element.set({ width: i.width, height: i.height, fill: new fabric.Pattern({ source: data2.value, repeat: 'no-repeat' }) })
+                                        }
+                                        canvas.requestRenderAll();
+                                    };
+                                    i.src = data2.value;
+                                }
+                            }
+                            canvas.requestRenderAll();
+                        } catch (error) {
+                        }
+                    });
+                });
+                sendToCasparcg(layerNumber)
+            });
         }
-
-        reader.readAsText(file);
+        else { alert(`${pageName} page not found in canvas list. Make a page with this name, add ${data.length}  text and set id of texts as ${data.map(val => { return val.key })} then update the page`) }
     }
 
-    return(
-        <form id='csv-form'>
-            <input
-                type='file'
-                accept='.csv'
-                id='csvFile'
-                onChange={(e) => {
-                    setCsvFile(e.target.files[0])
-                }}
-            >
-            </input>
-            <br/>
-            <button
-                onClick={(e) => {
-                    e.preventDefault()
-                    if(csvFile)submit()
-                }}
-            >
-                Submit
-            </button>
-            <br/>
-            <br/>
-            {csvArray.length>0 ? 
+    const sendToCasparcg = (layerNumber) => {
+        endpoint(`mixer ${window.chNumber}-${layerNumber} fill 0 0 0 1 6 ${window.animationMethod}`)
+        setTimeout(() => {
+            endpoint(`play ${window.chNumber}-${layerNumber} [HTML] xyz.html`);
+        }, 250);
+        setTimeout(() => {
+            endpoint(`call ${window.chNumber}-${layerNumber} "
+            var aa = document.createElement('div');
+            aa.style.position='absolute';
+            aa.innerHTML='${(canvas.toSVG()).replaceAll('"', '\\"')}';
+            document.body.appendChild(aa);
+            document.body.style.margin='0';
+            document.body.style.padding='0';
+            aa.style.zoom=(${currentscreenSize * 100}/1024)+'%';
+            document.body.style.overflow='hidden';
+            "`)
+        }, 300);
+        setTimeout(() => {
+            endpoint(`mixer ${window.chNumber}-${layerNumber} fill 0 0 1 1 10 ${window.animationMethod}`)
+        }, 800);
+
+        setTimeout(() => {
+            updateGraphics(layerNumber);
+        }, 1100);
+    }
+
+    const updateGraphics = layerNumber => {
+        endpoint(`call ${window.chNumber}-${layerNumber} "
+                aa.innerHTML='${(canvas.toSVG()).replaceAll('"', '\\"')}';
+                "`)
+    }
+    // const stopGraphics = layerNumber => {
+    //     endpoint(`mixer ${window.chNumber}-${layerNumber} fill 0 0 0 1 12 ${window.animationMethod}`)
+    //     setTimeout(() => {
+    //         endpoint(`stop ${window.chNumber}-${layerNumber}`)
+    //     }, 1000);
+    // }
+
+    return (<div>
+        <h3> CSV test</h3>
+        {csvArray.length > 0 ?
             <>
                 <table border='1'>
-                    {/* <thead>
-                        <th>Name</th>
-                        <th>Age</th>
-                        <th>Rank</th>
-                    </thead> */}
                     <tbody>
                         {
-                            csvArray.map((item, i) => (<tr>
-                              { item.split(',').map((val)=><td>{val}</td>)}
-                               </tr> ))
+                            csvArray.map((item, i) => (<tr key={i}>
+                                {item?.split(';').map((val,ii) => <td>{val}</td>)}
+                                {(i !== 0) && <td><button onClick={()=>{recallPage('200','csv',[{ key: 'ccgf0', value: 'val', type: 'text' }])}}>Show</button></td>}
+                            </tr>))
                         }
                     </tbody>
                 </table>
             </> : null}
-        </form>
-    );
+    </div>);
 
 }
--- client/src/UdpClock.jsx
+++ client/src/UdpClock.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
-import { address1 } from './common'
-import axios from 'axios';
+// import { address1 } from './common'
+// import axios from 'axios';
 
 const UdpClock = () => {
-    const startColock = () => {
-        const data = { string: 'string' }
-        axios.post(address1 + '/showUdpClock', data).then((aa) => {
-            //    console.log(aa)
-        }).catch((aa) => { console.log('Error', aa) });
-    }
+    // const startColock = () => {
+    //     const data = { string: 'string' }
+    //     axios.post(address1 + '/showUdpClock', data).then((aa) => {
+    //         //    console.log(aa)
+    //     }).catch((aa) => { console.log('Error', aa) });
+    // }
 
-    const stopColock = () => {
-        const data = { string: 'string' }
-        axios.post(address1 + '/stopUdpClock', data).then((aa) => {
-            //    console.log(aa)
-        }).catch((aa) => { console.log('Error', aa) });
-    }
+    // const stopColock = () => {
+    //     const data = { string: 'string' }
+    //     axios.post(address1 + '/stopUdpClock', data).then((aa) => {
+    //         //    console.log(aa)
+    //     }).catch((aa) => { console.log('Error', aa) });
+    // }
     return (
         <div>
-            UdpClock
+            {/* UdpClock
 
             <button onClick={startColock}>Show udp Clock template</button>
-            <button onClick={stopColock}>Stop udp Clock template</button>
+            <button onClick={stopColock}>Stop udp Clock template</button> */}
 
         </div>
     )
