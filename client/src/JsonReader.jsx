d332e234f36dadae9220dfbef7564e3f07d5d546
vimlesh1975 <vimlesh1975@hotmail.com>
2022-09-23 13:24:04 +0530

    updated canvas list

--- client/src/JsonReader.jsx
+++ client/src/JsonReader.jsx
@@ -327,8 +327,51 @@ const JsonReader = () => {
     }
     return (<div style={{ overflow: 'scroll', height: 900 }}>
         <div style={{ height: 200, overflow: 'scroll' }}>
-            Double click to add on canvas:<br />
-            <textarea onDoubleClick={getSelectionText} style={{ width: 750, height: 150 }}> fgfg</textarea>
+            Paste content Here and then Double click on any word to add on canvas:<br />
+            <textarea onDoubleClick={getSelectionText} style={{ width: 750, height: 150 }}>1	Archery	Ahmedabad	Sanskardham Venue 1	September 30-October 6 (7 days)
+                2	Kho-Kho	Ahmedabad	Sanskardham Venue 2	September 30-October 4 (5 days)
+                3	Mallakhambh	Ahmedabad	Sanskardham Venue 2	October 7-11 (5 days)
+                4	Rugby 7s	Ahmedabad	Transtadia, Football Ground	September 28-30 (3 days)
+                5	Football (M)	Ahmedabad	Transtadia, Football Ground	October 2-11 (10 days)
+                6	Football (F)	Ahmedabad	Shahibagh Police Ground	October 1-10 (10 days)
+                7	Kabaddi	Ahmedabad	EKA, Transtadia	September 26-October 1 (5 days)
+                8	Yogasana	Ahmedabad	EKA, Transtadia	October 6-11 (6 days)
+                9	Rowing	Ahmedabad	Sabarmati Venue 1	September 30-October 3 (4 days)
+                10	Canoeing	Ahmedabad	Sabarmati Venue 1	October 10-11 (2 days)
+                11	RS - Skateboarding	Ahmedabad	Sabarmati Venue 3	September 30-October 1 (2 days)
+                12	RS - Artistic Skating	Ahmedabad	Sabarmati Venue 3	September 30-October 1 (2 days)
+                13	RS - Inline Skating	Ahmedabad	Sabarmati Venue 3	September 30-October 1 (2 days)
+                14	RS - Speed Skating	Ahmedabad	Sabarmati Venue 3	September 30-October 2 (3 days)
+                15	Tennis	Ahmedabad	Sabarmati Venue 4	September 29-October 5 (7 days)
+                16	Soft Tennis	Ahmedabad	Sabarmati Venue 4	October 7-11 (5 days)
+                17	Lawn Bowl	Ahmedabad	Kensville Golf and Country Club	September 26-October 3 (8 days)
+                18	Golf	Ahmedabad	Kensville Golf and Country Club	October 6-9 (4 days)
+                19	Shooting (Shotgun)	Ahmedabad	Crowne Academy	September 30-October 7 (8 days)
+                20	Shooting	Ahmedabad	Rifle Club	September 29-October 3 (5 days)
+                21	Cycling (Road)	Gandhinagar	CHH Road	October 8-9 (2 days)
+                22	Weightlifting	Gandhinagar	Mahatma Mandir Venue 1	September 30-October 4 (5 days)
+                23	Judo	Gandhinagar	Mahatma Mandir Venue 1	October 7-11 (5 days)
+                24	Fencing	Gandhinagar	Mahatma Mandir Venue 2	September 30-October 4 (5 days)
+                25	Wushu	Gandhinagar	Mahatma Mandir Venue 2	October 8-11 (4 days)
+                26	Boxing	Gandhinagar	Mahatma Mandir Venue 3	October 5-12 (8 days)
+                27	Wrestling	Gandhinagar	Mahatma Mandir Venue 3	September 30-October 2 (3 days)
+                28	Triathlon	Gandhinagar	IIT Gandhinagar	September 30-October 2 (3 days)
+                29	Squash	Gandhinagar	IIT Gandhinagar Courts	October 1-5 (5 days)
+                30	Athletics	Gandhinagar	IIT Gandhinagar Ground	September 30-October 4 (5 days)
+                31	Softball	Gandhinagar	IIT Gandhinagar Ground	October 7-11 (5 days)
+                32	Beach Handball	Surat	Dumas Beach	September 30-October 4 (5 days)
+                33	Beach Volleyball	Surat	Dumas Beach	October 6-9 (4 days)
+                34	Table Tennis	Surat	PDDU Indoor Stadium	September 20-24 (5 days)
+                35	Badminton	Surat	PDDU Indoor Stadium	October 1-6 (6 days)
+                36	Gymnastics	Vadodara	Sama Sports Complex	September 30-October 4 (5 days)
+                37	Handball	Vadodara	Sama Sports Complex	October 7-12 (6 days)
+                38	Aquatics	Rajkot	Sardar Patel Complex	October 2-8 (7 days)
+                39	Hockey	Rajkot	Saurashtra U & DC Ground 1, 2	October 2-9 (8 days)
+                40	Netball	Bhavnagar	MPH, SAG	September 26-30 (5 days)
+                41	Basketball 3x3	Bhavnagar	Outdoor Courts, SAG	October 1-3 (3 days)
+                42	Basketball 5x5	Bhavnagar	Outdoor Courts, SAG	October 1-6 (6 days)
+                43	Volleyball	Bhavnagar	MPH, SAG	October 8-12 (5 days)
+                44	Cycling (Track)	Delhi	Velodrome	October 1-4 (4 days)</textarea>
         </div>
         <div style={{ border: '1px solid red' }}>
             <span>Open Json File:</span><input
