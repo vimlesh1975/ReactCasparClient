a3468b04d9c435fec6e385344a31da84e09981e9
vimlesh1975 <vimlesh1975@hotmail.com>
2022-10-12 15:00:22 +0530

    removed process.env

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from 'react-redux'
 import store from './store'
 import Drawing2 from './Drawing2';
@@ -16,21 +16,14 @@ window.store = store;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router >
       <Routes>
-        <Route exact path='/' element={<App />} />
-        <Route exact path='/drawing2' element={<Drawing2 canvasOutput={true} />} />
-        {/* <Route exact path='/threejs' element={<Threejs />} /> */}
-        <Route exact path='/threejs2' element={<Threejs2 />} />
+        <Route exact path='ReactCasparClient/' element={<App />} />
+        <Route exact path='ReactCasparClient/drawing2' element={<Drawing2 canvasOutput={true} />} />
+        <Route exact path='ReactCasparClient/threejs' element={<Threejs />} />
+        <Route exact path='ReactCasparClient/threejs2' element={<Threejs2 />} />
       </Routes>
     </Router>
-
-    <HashRouter>
-      <Routes>
-        <Route exact path='/' element={<Threejs />} />
-        {console.log(process.env.PUBLIC_URL)}
-      </Routes>
-    </HashRouter>
   </Provider>
 );
 
