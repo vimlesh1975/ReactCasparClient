5f2a4f6ffe14e837329d8f34621ed6afeb5e5aec
vimlesh1975 <vimlesh1975@hotmail.com>
2022-10-27 11:56:21 +0530

    removed extra packages

--- client/src/Threejs.jsx
+++ client/src/Threejs.jsx
@@ -4,7 +4,7 @@ const Threejs = () => {
     return (
         <div>
             <h1>ThreeJS</h1>
-            Online <a href='https://vimlesh1975.github.io/three/' target="_blank" rel="noreferrer" > Threejs</a>
+            Online <a href='https://vimlesh1975.github.io/three/' target="_blank" rel="noreferrer" > Threejs</a> <br />
             Offline- Double click Satrt_three.bat file from downloaded folder.
             Then
             <a href='http://localhost:11000/three' target="_blank" rel="noreferrer" > Threejs</a>
