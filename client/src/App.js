c0d1ceb43d6323d67364de6f82255288adbe4e46
vimlesh1975 <vimlesh1975@hotmail.com>
2022-12-05 16:57:40 +0530

    data change

--- client/src/App.js
+++ client/src/App.js
@@ -37,7 +37,7 @@ import ColorGradient2 from './ColorGradient2'
 import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
 import Threejs from './Threejs';
 
-const buildDate = '021222_1'
+const buildDate = '051222_1'
 
 const App = () => {
   const canvas = useSelector(state => state.canvasReducer.canvas);
