9fa5a50b47887a4674d82e1575ba9f63478b0073
vimlesh1975 <vimlesh1975@hotmail.com>
2023-01-25 17:23:08 +0530

    Play tharetrejs animation (not finish)

--- client/src/theatrejs/WebAnimator.jsx
+++ client/src/theatrejs/WebAnimator.jsx
@@ -1,18 +1,17 @@
 import React, { useEffect, useState } from 'react'
 
 import studio from '@theatre/studio'
-import { getProject, types } from '@theatre/core'
+import { getProject } from '@theatre/core'
 import DrawingforTheatrejs from '../DrawingforTheatrejs'
 import { useSelector } from 'react-redux'
 
-import { shadowOptions, endpoint, templateLayers } from '../common'
+import { endpoint, templateLayers } from '../common'
 
-/**
- * Theatre.js
- */
-const project = getProject('HTML Animation Tutorial', {
+const project = getProject('HTML Animation Tutorial', {})
 
-})
+
+
+
 const sheet = project.sheet('Sheet 1');
 
 const WebAnimator = ({ canvasObjects = { "version": "5.2.4", "objects": [{ "type": "ellipse", "version": "5.2.4", "originX": "left", "originY": "top", "left": 180, "top": 330, "width": 100, "height": 160, "fill": "#0000ff", "stroke": "#ffffff", "strokeWidth": 3, "strokeDashArray": null, "strokeLineCap": "butt", "strokeDashOffset": 0, "strokeLineJoin": "miter", "strokeUniform": true, "strokeMiterLimit": 4, "scaleX": 1, "scaleY": 1, "angle": 0, "flipX": false, "flipY": false, "opacity": 0.9, "shadow": { "color": "black", "blur": 30, "offsetX": 0, "offsetY": 0, "affectStroke": false, "nonScaling": false }, "visible": true, "backgroundColor": "", "fillRule": "nonzero", "paintFirst": "fill", "globalCompositeOperation": "source-over", "skewX": 0, "skewY": 0, "rx": 50, "ry": 80, "id": "ccg_11", "class": "class_11", "selectable": true }, { "type": "circle", "version": "5.2.4", "originX": "left", "originY": "top", "left": 150, "top": 0, "width": 200, "height": 200, "fill": "#0000ff", "stroke": "#ffffff", "strokeWidth": 3, "strokeDashArray": null, "strokeLineCap": "butt", "strokeDashOffset": 0, "strokeLineJoin": "miter", "strokeUniform": true, "strokeMiterLimit": 4, "scaleX": 1, "scaleY": 1, "angle": 0, "flipX": false, "flipY": false, "opacity": 1, "shadow": { "color": "black", "blur": 30, "offsetX": 0, "offsetY": 0, "affectStroke": false, "nonScaling": false }, "visible": true, "backgroundColor": "", "fillRule": "nonzero", "paintFirst": "fill", "globalCompositeOperation": "source-over", "skewX": 0, "skewY": 0, "radius": 100, "startAngle": 0, "endAngle": 360, "id": "ccg_12", "class": "class_12", "selectable": true }] } }) => {
@@ -30,13 +29,13 @@ const WebAnimator = ({ canvasObjects = { "version": "5.2.4", "objects": [{ "type
     }, [])
 
     const initialiseCore = () => {
-        const hexToRGB = hex => {
-            const red = parseInt(hex.slice(1, 3), 16)
-            const green = parseInt(hex.slice(3, 5), 16)
-            const blue = parseInt(hex.slice(5, 7), 16)
-            return { r: red / 255, g: green / 255, b: blue / 255, a: 1 } // return an object
-            // return [ r, g, b ]
-        }
+        // const hexToRGB = hex => {
+        //     const red = parseInt(hex.slice(1, 3), 16)
+        //     const green = parseInt(hex.slice(3, 5), 16)
+        //     const blue = parseInt(hex.slice(5, 7), 16)
+        //     return { r: red / 255, g: green / 255, b: blue / 255, a: 1 } // return an object
+        //     // return [ r, g, b ]
+        // }
         var mouseDown = 0;
         document.body.onmousedown = function () {
             mouseDown = 1;
@@ -49,22 +48,22 @@ const WebAnimator = ({ canvasObjects = { "version": "5.2.4", "objects": [{ "type
             canvas.getObjects().forEach(element => {
                 const obj = sheet.object(element.id, {
                     left: element.left,
-                    top: element.top,
-                    width: element.width,
-                    height: element.height,
-                    opacity: types.number(element.opacity, { nudgeMultiplier: 0.1 }),
-                    scaleX: types.number(element.scaleX, { nudgeMultiplier: 0.01 }),
-                    scaleY: types.number(element.scaleY, { nudgeMultiplier: 0.01 }),
-                    angle: element.angle,
-                    fill: types.rgba(hexToRGB(element.fill ? element.fill : '#ff0000')),
-                    rx: types.number(element.rx ? element.rx : 10, { range: [0, 100] }),
-                    ry: types.number(element.ry ? element.rx : 10, { range: [0, 100] }),
-                    strokeWidth: types.number(element.strokeWidth, { range: [0, 100] }),
-                    stroke: types.rgba(element.stroke ? hexToRGB(element.stroke) : hexToRGB('#000000')),
-                    shadow: { ...shadowOptions, color: types.rgba(hexToRGB(element.shadow.color)), blur: types.number(parseInt(element.shadow.blur), { range: [0, 100] }) },
-                    fontSize: types.number(element.fontSize ? parseInt(element.fontSize) : 30, { range: [0, 100] }),
-                    strkdsar: types.number(element.strokeDashArray ? parseInt(element.strokeDashArray) : 0, { range: [0, 1000] }),
-                    strkDsOfst: types.number(element.strokeDashOffset ? parseInt(element.strokeDashOffset) : 0, { range: [-1000, 1000] }),
+                    // top: element.top,
+                    // width: element.width,
+                    // height: element.height,
+                    // opacity: types.number(element.opacity, { nudgeMultiplier: 0.1 }),
+                    // scaleX: types.number(element.scaleX, { nudgeMultiplier: 0.01 }),
+                    // scaleY: types.number(element.scaleY, { nudgeMultiplier: 0.01 }),
+                    // angle: element.angle,
+                    // fill: types.rgba(hexToRGB(element.fill ? element.fill : '#ff0000')),
+                    // rx: types.number(element.rx ? element.rx : 10, { range: [0, 100] }),
+                    // ry: types.number(element.ry ? element.rx : 10, { range: [0, 100] }),
+                    // strokeWidth: types.number(element.strokeWidth, { range: [0, 100] }),
+                    // stroke: types.rgba(element.stroke ? hexToRGB(element.stroke) : hexToRGB('#000000')),
+                    // shadow: { ...shadowOptions, color: types.rgba(hexToRGB(element.shadow.color)), blur: types.number(parseInt(element.shadow.blur), { range: [0, 100] }) },
+                    // fontSize: types.number(element.fontSize ? parseInt(element.fontSize) : 30, { range: [0, 100] }),
+                    // strkdsar: types.number(element.strokeDashArray ? parseInt(element.strokeDashArray) : 0, { range: [0, 1000] }),
+                    // strkDsOfst: types.number(element.strokeDashOffset ? parseInt(element.strokeDashOffset) : 0, { range: [-1000, 1000] }),
                 });
 
 
@@ -73,9 +72,9 @@ const WebAnimator = ({ canvasObjects = { "version": "5.2.4", "objects": [{ "type
                     if (mouseDown === 1) {
                         studio.transaction(({ set }) => {
                             set(obj.props.left, event.target.left);
-                            set(obj.props.top, event.target.top);
-                            set(obj.props.width, event.target.width);
-                            set(obj.props.angle, event.target.angle);
+                            // set(obj.props.top, event.target.top);
+                            // set(obj.props.width, event.target.width);
+                            // set(obj.props.angle, event.target.angle);
                         });
                     }
 
@@ -83,8 +82,8 @@ const WebAnimator = ({ canvasObjects = { "version": "5.2.4", "objects": [{ "type
                 const onScaling = (obj, event) => {
                     // console.log(event)
                     studio.transaction(({ set }) => {
-                        set(obj.props.scaleX, event.transform.target.scaleX);
-                        set(obj.props.scaleY, event.transform.target.scaleY);
+                        // set(obj.props.scaleX, event.transform.target.scaleX);
+                        // set(obj.props.scaleY, event.transform.target.scaleY);
                     });
                 };
                 const onMousedblclick = (obj, event) => {
@@ -101,28 +100,30 @@ const WebAnimator = ({ canvasObjects = { "version": "5.2.4", "objects": [{ "type
                 obj.onValuesChange((obj) => {
                     element.set({
                         left: obj.left,
-                        top: obj.top,
-                        width: obj.width,
-                        height: obj.height,
-                        opacity: obj.opacity,
-                        scaleX: obj.scaleX,
-                        scaleY: obj.scaleY,
-                        angle: obj.angle,
-                        fill: obj.fill,
-                        rx: obj.rx,
-                        ry: obj.ry,
-                        strokeWidth: obj.strokeWidth,
-                        stroke: obj.stroke,
-                        shadow: obj.shadow,
-                        fontSize: obj.fontSize,
-                        strokeDashArray: [obj.strkdsar, obj.strkdsar],
-                        strokeDashOffset: obj.strkDsOfst
+                        // top: obj.top,
+                        // width: obj.width,
+                        // height: obj.height,
+                        // opacity: obj.opacity,
+                        // scaleX: obj.scaleX,
+                        // scaleY: obj.scaleY,
+                        // angle: obj.angle,
+                        // fill: obj.fill,
+                        // rx: obj.rx,
+                        // ry: obj.ry,
+                        // strokeWidth: obj.strokeWidth,
+                        // stroke: obj.stroke,
+                        // shadow: obj.shadow,
+                        // fontSize: obj.fontSize,
+                        // strokeDashArray: [obj.strkdsar, obj.strkdsar],
+                        // strokeDashOffset: obj.strkDsOfst
                     });
                     element.setCoords();
                     canvas.requestRenderAll();
                 });
-
             })
+            project.ready.then(() => {
+                sheet.sequence.play({ iterationCount: Infinity, range: [0, 2] });
+            });
         })
 
 
@@ -131,15 +132,61 @@ const WebAnimator = ({ canvasObjects = { "version": "5.2.4", "objects": [{ "type
         localStorage.removeItem("theatre-0.4.persistent");
         window.location.reload();
     }
+
+
     const playtoCasparcg = (layerNumber = templateLayers.theatrejs) => {
         endpoint(`play ${1}-${templateLayers.theatrejs} [HTML] xyz.html`);
-        const dd = (canvas.toSVG(['id', 'class', 'selectable'])).replaceAll('"', '\\"')
-        console.log(dd)
-        const script = `"const aa=document.createElement('div');
-        document.body.appendChild(aa);
-        aa.innerHTML=\`${dd}\`;
+
+        const content = JSON.stringify(canvas.toJSON());
+        const content2 = content.replaceAll('"', '\\"');
+        var script1 = `"
+        var script = document.createElement('script');
+        script.src = 'https://cdnjs.cloudflare.com/ajax/libs/fabric.js/6.0.0-rc.1/fabric.min.js';
+        document.head.appendChild(script);  
         "`
-        endpoint(`call 1-166 ${script}`)
+        setTimeout(() => {
+            endpoint(`call 1-166 ${script1}`)
+        }, 100);
+
+        var script3 = `"
+        document.body.innerHTML += \`<canvas id='canvas' width='1920' height='1080'></canvas>;\`;
+        var canvas = new fabric.Canvas('canvas');
+        const content =\`${content2}\`;
+        var script = document.createElement('script');
+        script.src = 'https://cdn.jsdelivr.net/npm/@theatre/browser-bundles@0.6.0-dev.4/dist/core-only.min.js';
+        document.head.appendChild(script);
+        "`
+        setTimeout(() => {
+            endpoint(`call 1-166 ${script3}`)
+        }, 2000);
+
+        const state1 = (JSON.stringify(studio.createContentOfSaveFile('HTML Animation Tutorial')));
+
+        var script4 = `"
+       const { core } = Theatre;
+       const project = core.getProject('HTML Animation Tutorial', {state:${(state1.replaceAll('"', '\\"')).replaceAll('\\\\"', "'")}});
+       const sheet = project.sheet('Sheet 1');
+       canvas.loadFromJSON(content,()=>{
+        canvas.getObjects().forEach(element => {
+            var obj = sheet.object(element.id, {
+                left: element.left,
+            });
+            obj.onValuesChange((obj) => {
+                element.set({
+                    left: obj.left,
+                });
+                canvas.renderAll();
+            });
+        })
+        });
+
+        project.ready.then(() => {
+            sheet.sequence.play({ iterationCount: Infinity, range: [0, 2] });
+        });
+        "`
+        setTimeout(() => {
+            endpoint(`call 1-166 ${script4}`)
+        }, 3000);
 
     }
 
