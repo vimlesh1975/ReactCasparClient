1c25ab3f1afdd62062d00257c84342c4fa1324eb
vimlesh1975 <vimlesh1975@hotmail.com>
2023-01-27 13:41:42 +0530

    removed zoom pan and context menu

--- client/src/DrawingforTheatrejs.jsx
+++ client/src/DrawingforTheatrejs.jsx
@@ -1,5 +1,4 @@
 import { FabricJSCanvas, useFabricJSEditor } from "fabricjs-react";
-import ContextMenu from './ContextMenu'
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux'
 
@@ -10,65 +9,10 @@ const DrawingforTheatrejs = () => {
 
     window.dispatch = dispatch;
     window.editor = editor;
-    function cancesetZoomAndPan(canvas) {
-        canvas.on('mouse:wheel', null);
-        canvas.on('mouse:down', null);
-        canvas.on('mouse:move', null);
-        canvas.on('mouse:up', null);
-    }
 
-    function setZoomAndPan(canvas) {
-        canvas.on('mouse:wheel', function (opt) {
-            var delta = opt.e.deltaY;
-            var zoom = canvas.getZoom();
-            zoom *= 0.999 ** delta;
-            if (zoom > 20) zoom = 20;
-            if (zoom < 0.01) zoom = 0.01;
-            // canvas.setZoom(zoom);
-            canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
-            opt.e.preventDefault();
-            opt.e.stopPropagation();
-        })
 
-        canvas.on('mouse:down', function (opt) {
-            var evt = opt.e;
-            if (evt.altKey === true) {
-                this.isDragging = true;
-                this.selection = false;
-                this.lastPosX = evt.clientX;
-                this.lastPosY = evt.clientY;
-            }
-        });
-        canvas.on('mouse:move', function (opt) {
-            if (this.isDragging) {
-                var e = opt.e;
-                var vpt = this.viewportTransform;
-                vpt[4] += e.clientX - this.lastPosX;
-                vpt[5] += e.clientY - this.lastPosY;
-                this.requestRenderAll();
-                this.lastPosX = e.clientX;
-                this.lastPosY = e.clientY;
-            }
-        });
-        canvas.on('mouse:up', function (opt) {
-            // on mouse up we want to recalculate new interaction
-            // for all objects, so we call setViewportTransform
-            this.setViewportTransform(this.viewportTransform);
-            this.isDragging = false;
-            this.selection = true;
-        });
-    }
 
-    useEffect(() => {
-        setTimeout(() => {
-            window.editor.canvas.extraProps = ['id', 'selectable']
-            setZoomAndPan(window.editor.canvas);
-            window.editor.canvas.preserveObjectStacking = true;
-        }, 2000);
-        return () => {
-            cancesetZoomAndPan(window.editor.canvas)
-        }
-    }, [])
+
 
     useEffect(() => {
         dispatch({ type: 'CHANGE_CANVAS', payload: editor?.canvas })
@@ -77,7 +21,7 @@ const DrawingforTheatrejs = () => {
 
     return (<div>
         <FabricJSCanvas className={'DrawingforTheatrejs'} onReady={onReady} />
-        <ContextMenu canvas={editor?.canvas} />
+        {/* <ContextMenu canvas={editor?.canvas} /> */}
     </div>);
 };
 export default DrawingforTheatrejs;
