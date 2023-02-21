4631058a3d80d3782bf0dab9bdb95272a0ce66cb
vimlesh1975 <vimlesh1975@hotmail.com>
2023-02-21 10:52:26 +0530

    All default canvaslist unoque id done

--- client/src/common.js
+++ client/src/common.js
@@ -3,7 +3,7 @@ import { animation } from './animation.js'
 import { fabric } from "fabric";
 
 
-export const buildDate = '200223_2'
+export const buildDate = '210223_1'
 
 export const hexToRGB = hex => {
     const red = parseInt(hex.slice(1, 3), 16)
