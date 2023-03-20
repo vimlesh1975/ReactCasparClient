83cb0fd0b981b140c5fd6af7ee4dc83385aecf38
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2023-03-20 22:30:24 +0530

    command line tsparticles

--- client/src/tsparticles/Tsparticles1.jsx
+++ client/src/tsparticles/Tsparticles1.jsx
@@ -24,13 +24,136 @@ const Tsparticles1 = () => {
 
     const particlesLoaded = useCallback(async (container) => {
         await console.log(container);
+        window.tsParticles = container._engine;
     }, []);
     const test = (layerNumber) => {
         endpoint(`play 1-${layerNumber} [html] "http://localhost:10000/ReactCasparClient/Tsparticles1"`);
     }
+
+    const test2 = (layerNumber) => {
+        const aa = ` tsParticles.load("particles-canvas", 
+
+        {
+        "particles": {
+          "number": {
+            "value": 10,
+            "density": {
+              "enable": false,
+              "value_area": 2
+            }
+          },
+          "color": {
+            "value": "#023a46"
+          },
+          "shape": {
+            "type": "polygon",
+            "stroke": {
+              "width": 5,
+              "color": "#d41d1d"
+            },
+            "polygon": {
+              "nb_sides": 6
+            },
+            "image": {
+              "src": "img/github.svg",
+              "width": 40,
+              "height": 20
+            }
+          },
+          "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+              "enable": true,
+              "speed": 1,
+              "opacity_min": 0.1,
+              "sync": false
+            }
+          },
+          "size": {
+            "value": 20,
+            "random": true,
+            "anim": {
+              "enable": true,
+              "speed": 1,
+              "size_min": 200,
+              "sync": true
+            }
+          },
+          "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#000000",
+            "opacity": 1,
+            "width": 10
+          },
+          "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+              "enable": false,
+              "rotateX": 600,
+              "rotateY": 1200
+            }
+          }
+        },
+        "interactivity": {
+          "detect_on": "canvas",
+          "events": {
+            "onhover": {
+              "enable": true,
+              "mode": "repulse"
+            },
+            "onclick": {
+              "enable": true,
+              "mode": "push"
+            },
+            "resize": true
+          },
+          "modes": {
+            "grab": {
+              "distance": 400,
+              "line_linked": {
+                "opacity": 1
+              }
+            },
+            "bubble": {
+              "distance": 400,
+              "size": 40,
+              "duration": 2,
+              "opacity": 8,
+              "speed": 3
+            },
+            "repulse": {
+              "distance": 200,
+              "duration": 0.4
+            },
+            "push": {
+              "particles_nb": 40
+            },
+            "remove": {
+              "particles_nb": 20
+            }
+          }
+        },
+        "retina_detect": true
+      }
+        
+        );`
+        const bb = aa.replaceAll('"', '\\"');
+        endpoint(`call 1-${layerNumber} "
+        ${bb}
+        "`);
+    }
     return (<div onMouseOver={() => setLoaded(true)}>
         {loaded ? <div >
             <button onClick={() => test(templateLayers.Tsparticles1)}>Test</button>
+            <button onClick={() => test2(templateLayers.Tsparticles1)}>Test</button>
         </div> : ''}
 
         <Particles
