094f0976ec3013e902aaa4441f99251fbdabc8f0
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2023-06-23 16:29:35 +0530

    url in input

--- client/src/streaming/Streaming.jsx
+++ client/src/streaming/Streaming.jsx
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { endpoint, streamingAddress } from "../common";
 import VideoPlayer from "./VideoPlayer ";
 
 const Streaming = () => {
+  const [streamingUrl, setStreamingUrl] = useState(
+    "add 1 stream rtmp://localhost/live/STREAM_NAME -codec:v libx264 -codec:a aac -strict -2  -b:a 128k -x264opts:v keyint=45 -ar:a 48000 -tune:v zerolatency -preset:v ultrafast -b:v 1500k -minrate:v 1500k -maxrate:v 1500k -bufsize:v 1500k -filter:v format=pix_fmts=yuv420p,scale=400:300 -filter:a pan=stereo|c0=c0|c1=c1 -format flv"
+  );
+
   const starRtmpStreaming = () => {
-    endpoint(
-      "add 1 stream rtmp://localhost/live/STREAM_NAME -codec:v libx264 -codec:a aac -strict -2  -b:a 128k -x264opts:v keyint=45 -ar:a 48000 -tune:v zerolatency -preset:v ultrafast -b:v 1500k -minrate:v 1500k -maxrate:v 1500k -bufsize:v 1500k -filter:v format=pix_fmts=yuv420p,scale=400:300 -filter:a pan=stereo|c0=c0|c1=c1 -format flv"
-    );
+    endpoint(streamingUrl);
   };
 
   return (
     <div>
-      <button onClick={starRtmpStreaming}>
-        Start rtmp Streaming from Casparcg{" "}
-      </button>
+      <div>
+        <button onClick={starRtmpStreaming}>
+          Start rtmp Streaming from Casparcg{" "}
+        </button>{" "}
+        <strong>Url:</strong>
+      </div>
+      <textarea
+        value={streamingUrl}
+        type="text"
+        style={{ width: 550, height: 80 }}
+        onChange={(e) => setStreamingUrl(e.target.value)}
+        multiple={true}
+      />
+
       <VideoPlayer sourceUrl={streamingAddress() + "/live/STREAM_NAME.flv"} />
     </div>
   );
--- client/src/streaming/VideoPlayer .jsx
+++ client/src/streaming/VideoPlayer .jsx
@@ -1,56 +1,34 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import videojs from "video.js";
 import "video.js/dist/video-js.css";
 import flvjs from "flv.js";
 var flvPlayer;
 const VideoPlayer = ({ sourceUrl }) => {
   const videoRef = useRef(null);
-
-  useEffect(() => {
-    let player;
-
-    const setupPlayer = () => {
-      const videoElement = videoRef.current;
-      player = videojs(videoElement, {
-        html5: {
-          hls: {
-            overrideNative: true,
-          },
-        },
-      });
-
-      flvPlayer = flvjs.createPlayer({
-        type: "flv",
-        url: sourceUrl,
-      });
-
-      //   player.src({
-      //     src: sourceUrl,
-      //     type: "video/flv",
-      //   });
-
-      flvPlayer.attachMediaElement(videoElement);
-      //   flvPlayer.load();
-      //   flvPlayer.play();
-    };
-
-    setupPlayer();
-
-    return () => {
-      if (player) {
-        player.dispose();
-      }
-    };
-  }, [sourceUrl]);
+  const [streamingUrl, setStreamingUrl] = useState(sourceUrl);
 
   const preview = () => {
+    const videoElement = videoRef.current;
+    videojs(videoElement, {
+      html5: {
+        hls: {
+          overrideNative: true,
+        },
+      },
+    });
+
+    flvPlayer = flvjs.createPlayer({
+      type: "flv",
+      url: streamingUrl,
+    });
+
+    flvPlayer.attachMediaElement(videoElement);
     flvPlayer.load();
     flvPlayer.play();
   };
 
   return (
     <>
-      <button onClick={preview}>Preview</button>
       <div>
         <video
           ref={videoRef}
@@ -58,6 +36,14 @@ const VideoPlayer = ({ sourceUrl }) => {
           width={500}
         />
       </div>
+      <strong>Url:</strong>
+      <input
+        value={streamingUrl}
+        type="text"
+        style={{ width: 450 }}
+        onChange={(e) => setStreamingUrl(e.target.value)}
+      />
+      <button onClick={preview}>Preview</button>
     </>
   );
 };
