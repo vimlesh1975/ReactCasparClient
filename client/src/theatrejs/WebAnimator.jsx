78aeb8284d571457e63e54457579d5bbc9a7183f
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2023-07-05 15:46:01 +0530

    Delete keyframes all and indivisual

--- client/src/theatrejs/WebAnimator.jsx
+++ client/src/theatrejs/WebAnimator.jsx
@@ -26,7 +26,7 @@ function getKeyframes(sheet, tracks, objectKey) {
     const json = studio.createContentOfSaveFile(sheet.address.projectId);
     const { trackData, trackIdByPropPath } = json.sheetsById[sheet.address.sheetId].sequence.tracksByObject[objectKey];
     return tracks.reduce((result, track) => {
-        result[track] = trackData[trackIdByPropPath[`["${track}"]`]].keyframes.map(k => {
+        result[track] = trackData[trackIdByPropPath[`["${track}"]`]]?.keyframes?.map(k => {
             return { position: k.position, value: k.value };
         });
         return result;
@@ -338,7 +338,37 @@ const WebAnimator = () => {
                         })
                     }}>Reset All to Default</li>
 
-                    <li onClick={deleteItem}>Delete</li>
+                    <li onClick={deleteItem}>Delete Object</li>
+                    <li>Delete KeyFrames<ul>
+                        <li onClick={() => {
+                            const tracks = Object.keys(getObjectbyId(studio.selection[0].address.objectKey).value)// ['left', 'top']
+                            // const tracks = ['left', 'top']
+                            const keyframes = getKeyframes(sheet, tracks, studio.selection[0].address.objectKey);
+                            studio.transaction((api) => {
+                                tracks.forEach((track) => {
+                                    keyframes[track]?.forEach((val) => {
+                                        sheet.sequence.position = val.position
+                                        api.unset(getObjectbyId(studio.selection[0].address.objectKey).props[track]);
+                                    })
+                                })
+                            })
+                        }}>All</li>
+                        {getObjectbyId(studio?.selection?.[0]?.address?.objectKey)?.value && (Object.keys(getObjectbyId(studio?.selection?.[0]?.address?.objectKey)?.value))?.map((val1 => {
+                            return <li onClick={() => {
+                                const tracks = [val1]
+                                const keyframes = getKeyframes(sheet, tracks, studio.selection[0].address.objectKey);
+                                studio.transaction((api) => {
+                                    tracks.forEach((track) => {
+                                        keyframes[track]?.forEach((val) => {
+                                            sheet.sequence.position = val.position
+                                            api.unset(getObjectbyId(studio.selection[0].address.objectKey).props[track]);
+                                        })
+                                    })
+                                })
+                            }
+                            }>{val1}</li>
+                        }))}
+                    </ul></li>
                     <li>Text Align<ul >
                         <li onClick={() => alignLeft(canvas)}>Left</li>
                         <li onClick={() => alignRight(canvas)}>Right</li>
@@ -2071,20 +2101,6 @@ img/flag/Morocco.png,Viresh Kumar,50,Kviresh10@gmail.com`;
                 dispatch({ type: 'CHANGE_CURRENTSCREENSIZE', payload: parseInt(e.target.value) })
             }
             }>  {screenSizes.map((val) => { return <option key={uuidv4()} value={val}>{val}</option> })} </select>
-            <button onClick={() => {
-                const tracks = ['left', 'top']
-                const keyframes = getKeyframes(sheet, tracks, studio.selection[0].address.objectKey);
-                studio.transaction((api) => {
-                    tracks.forEach((track) => {
-                        keyframes[track].forEach((val) => {
-                            sheet.sequence.position = val.position
-                            api.unset(getObjectbyId(studio.selection[0].address.objectKey).props[track]);
-                        })
-                    })
-                })
-                console.log(studio.selection[0].address.objectKey)
-            }}>test</button>
-
             <div style={{ position: 'absolute', left: 1540, top: 25, zIndex: 101, backgroundColor: 'white', display: !showSavePannel ? 'none' : '' }}>
                 <Tabs forceRenderTabPanel={true} >
                     <TabList>
