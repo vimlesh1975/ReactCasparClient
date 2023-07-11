5d62d1ff0561b04128e7d8f0e8b3266354a2bf4e
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2023-07-11 20:59:19 +0530

    div

--- client/src/HtmlOutput.jsx
+++ client/src/HtmlOutput.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import { useSelector } from "react-redux";
 
-
 const HtmlOutput = () => {
     const clientId = useSelector((state) => state.clientIdReducer.clientId);
 
     return (
-        // <Html />
-        <div style={{ position: 'absolute' }}>
-            <iframe style={{ backgroundColor: 'red', overflow: 'hidden', width: 1920, height: 1080, transform: 'scale(.28)', transformOrigin: '0 0' }} src={"/ReactCasparClient/html/" + clientId} title='HtmlOutput'></iframe>
+        <div style={{ position: 'absolute', width: 1920, height: 1080, transform: 'scale(.28)', transformOrigin: '0 0' }}>
+            <iframe style={{ backgroundColor: 'grey', width: 1920, height: 1080, }} src={"/ReactCasparClient/html/" + clientId} title='HtmlOutput'></iframe>
 
         </div>
-
     )
 }
 
