fe5fa2ecaedadc554bb320c4af6ba681f0da7943
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2024-05-30 11:32:06 +0530

    for test done

--- client/src/data/EditableTable.jsx
+++ client/src/data/EditableTable.jsx
@@ -3,43 +3,45 @@ import { useSelector, useDispatch } from 'react-redux'
 
 const EditableTable = () => {
     const canvas = useSelector(state => state.canvasReducer.canvas);
-    // const layers = useSelector(state => state.canvasReducer.canvas?.getObjects());
     const dispatch = useDispatch();
     const [data1, setData1] = useState([]);
+    const [headers, setHeaders] = useState([]);
 
-    const handleChange = (e, i) => {
+    const handleChange = (e, key, rowIndex) => {
         const aa = [...data1];
-        aa[i] = e.target.value;
+        aa[rowIndex][key] = e.target.value;
         setData1(aa);
     };
 
-    const setText = () => {
+    const setText = (rowIndex) => {
         canvas.getObjects().forEach((element, i) => {
-            // if (element.id === id) {
-            element.text = data1[i];
-            // }
+            element.text = data1[rowIndex][element.id];
         });
         canvas.requestRenderAll();
         dispatch({ type: 'CHANGE_CANVAS', payload: canvas })
     }
 
     const createTable = () => {
-        const aa = [];
-        canvas.getObjects().forEach(element => {
-            aa.push([{ data: element.text + "1" }])
-        });
-        setData1(aa)
+        setHeaders(canvas.getObjects().map((elements) => elements.id));
+        const aa = canvas.getObjects().reduce((acc, obj) => {
+            if (obj.type === 'textbox' && obj.id != null) {
+                acc[obj.id] = obj.text;
+            }
+            return acc;
+        }, {});
+        setData1([aa]);
     }
 
     const addRows = () => {
-        const aa = [...data1];
-        canvas.getObjects().forEach(element => {
-            aa.push({ data: element.text + "2" })
+        const aa = canvas.getObjects().reduce((acc, obj) => {
+            if (obj.type === 'textbox' && obj.id != null) {
+                acc[obj.id] = obj.text;
+            }
+            return acc;
+        }, {});
 
-        });
-        setData1(aa)
+        setData1(data1 => [...data1, aa]);
     }
-
 
     return (<div>
         <button onClick={createTable}>Create Table</button>
@@ -47,28 +49,25 @@ const EditableTable = () => {
         <table border='1' >
             <thead>
                 <tr>
-                    {canvas?.getObjects()?.map((val, i) => <th key={i}>{val['id']}</th>)}
+                    {headers?.map((val, i) => <th key={i}>{val}</th>)}
                     <th>Play</th>
                 </tr>
             </thead>
             <tbody>
-
-
-                {data1?.map((element, i) => (<>
-                    <tr >
-                        <td key={i}>
-                            <input
-                                type="text"
-                                value={element[0].data}
-                                onChange={(e) => handleChange(e, i)}
-                            />
-                        </td>
-                        <td><button onClick={() => setText()}>Play</button></td>
+                {data1.map((row, rowIndex) => (
+                    <tr key={rowIndex}>
+                        {Object.entries(row).map(([key, value]) => (
+                            <td key={key}>
+                                <input
+                                    type="text"
+                                    value={value}
+                                    onChange={e => handleChange(e, key, rowIndex)}
+                                />
+                            </td>
+                        ))}
+                        <td><button onClick={() => setText(rowIndex)}>Play</button></td>
                     </tr>
-                </>))}
-
-
-
+                ))}
             </tbody>
         </table >
     </div>);
