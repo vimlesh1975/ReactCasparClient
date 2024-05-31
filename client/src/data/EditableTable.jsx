fa6fa9f2b309b750bb4700be96ed4781e8d560e6
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2024-05-31 13:18:58 +0530

    gsap player

--- client/src/data/EditableTable.jsx
+++ client/src/data/EditableTable.jsx
@@ -2,10 +2,14 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { saveFile, sendToCasparcg, templateLayers, stopGraphics, updateGraphics } from '../common'
 import Papa from "papaparse";
+import { fabric } from "fabric";
+
 import {
     FaPlay,
     FaStop,
 } from "react-icons/fa";
+import GsapPlayer from "../GsapPlayer";
+
 
 const EditableTable = () => {
     const canvas = useSelector(state => state.canvasReducer.canvas);
@@ -24,7 +28,21 @@ const EditableTable = () => {
 
     const setText = (rowIndex) => {
         canvas.getObjects().forEach((element, i) => {
-            element.text = data1[rowIndex][element.id];
+            if (element.type === 'textbox') {
+                element.text = data1[rowIndex][element.id];
+            }
+            if (element.type === 'image') {
+                const myelement = element;
+                fabric.Image.fromURL(data1[rowIndex][element.id], img => {
+                    img.set({ scaleX: myelement.width / img.width, scaleY: (myelement.height / img.height) })
+                    img.cloneAsImage(img1 => {
+                        myelement.setSrc(img1.getSrc(), () => {
+                            myelement.set({ visible: true });
+                            canvas.requestRenderAll();
+                        })
+                    })
+                })
+            }
         });
         canvas.requestRenderAll();
         dispatch({ type: 'CHANGE_CANVAS', payload: canvas })
@@ -32,13 +50,21 @@ const EditableTable = () => {
 
 
     const createTable = () => {
-        setHeaders(canvas.getObjects()
-            .filter((element) => element.type === 'textbox')
-            .map((element) => element.id)
+        setHeaders(
+            canvas.getObjects()
+                .filter((element) => (element.type === 'textbox' || element.type === 'image') && element.id != null)
+                .map((element) => element.id)
         );
-        const aa = canvas.getObjects().reduce((acc, obj) => {
-            if (obj.type === 'textbox' && obj.id != null) {
-                acc[obj.id] = obj.text + '0';
+
+        const aa = canvas.getObjects().reduce((acc, element) => {
+            if ((element.type === 'textbox' || element.type === 'image') && element.id != null) {
+                if (element.type === 'textbox') {
+                    acc[element.id] = element.text + '0';
+                }
+                if (element.type === 'image') {
+                    acc[element.id] = element.src;
+                }
+
             }
             return acc;
         }, {});
@@ -46,14 +72,19 @@ const EditableTable = () => {
     }
 
     const addRows = () => {
-        const aa = canvas.getObjects().reduce((acc, obj) => {
-            if (obj.type === 'textbox' && obj.id != null) {
-                acc[obj.id] = obj.text + data1.length;
+        const aa = canvas.getObjects().reduce((acc, element) => {
+            if ((element.type === 'textbox' || element.type === 'image') && element.id != null) {
+                if (element.type === 'textbox') {
+                    acc[element.id] = element.text + data1.length;
+                }
+                if (element.type === 'image') {
+                    acc[element.id] = element.src;
+                }
+
             }
             return acc;
         }, {});
-
-        setData1(data1 => [...data1, aa]);
+        setData1([...data1, aa]);
     }
 
     const deleteData = (rowIndex) => {
@@ -62,7 +93,14 @@ const EditableTable = () => {
         setData1(aa);
     }
     const createCSV = () => {
-        const rows = data1.map(row => headers.map(header => row[header]).join(','));
+        // const rows = data1.map(row => headers.map(header => row[header]).join(','));
+        const rows = data1.map(row => {
+            return headers.map(header => {
+                const value = row[header];
+                // Escape values containing commas by enclosing them in double quotes
+                return typeof value === 'string' && value.includes(',') ? `"${value}"` : value;
+            }).join(',');
+        });
         const data = [headers.join(','), ...rows].join('\n')
         const ss = new Date().toLocaleTimeString('en-US', { year: "numeric", month: "numeric", day: "numeric", hour12: false, hour: "numeric", minute: "numeric", second: "numeric" });
 
@@ -103,6 +141,25 @@ const EditableTable = () => {
         }
     };
 
+    const handleImageChange = (e, rowIndex, key) => {
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setData1(prevData => {
+                    const newData = [...prevData];
+                    newData[rowIndex][key] = reader.result;
+                    return newData;
+                });
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+
+    const handleImageDoubleClick = (rowIndex, key) => {
+        // Trigger the hidden file input click
+        document.getElementById(`fileInput-${rowIndex}-${key}`).click();
+    };
 
 
     return (<div>
@@ -114,7 +171,9 @@ const EditableTable = () => {
             Layer:<input type='number' value={dataLayer} onChange={e => setDataLayer(e.target.value)} style={{ width: 50 }} />
             <button style={{ fontSize: 25, backgroundColor: 'red' }} onClick={() => stopGraphics(dataLayer)}><FaStop /></button>
 
+
         </div>
+
         <div style={{ maxWidth: 900, maxHeight: 600, overflow: 'auto' }}>
             <table>
                 <thead>
@@ -126,22 +185,40 @@ const EditableTable = () => {
                 </thead>
                 <tbody>
                     {data1.map((row, rowIndex) => (
-
                         <tr key={rowIndex}>
                             <td><button onClick={() => deleteData(rowIndex)}>Delete</button></td>
                             {Object.entries(row).map(([key, value]) => (
                                 <td key={key}>
-                                    <input
-                                        type="text"
-                                        value={value}
-                                        onChange={e => handleChange(e, key, rowIndex)}
-                                    />
+                                    {typeof value === 'string' && value.startsWith('data:image/') ? (
+                                        <>
+                                            <img
+                                                src={value}
+                                                alt="Profile"
+                                                style={{ width: 30, height: 25, cursor: 'pointer' }}
+                                                onClick={() => handleImageDoubleClick(rowIndex, key)}
+                                            />
+                                            <input
+                                                type="file"
+                                                id={`fileInput-${rowIndex}-${key}`}
+                                                style={{ display: 'none' }}
+                                                onChange={(e) => handleImageChange(e, rowIndex, key)}
+                                            />
+                                        </>
+                                    ) : (
+                                        <input
+                                            type="text"
+                                            value={value}
+                                            onChange={e => handleChange(e, key, rowIndex)}
+                                        />
+                                    )}
                                 </td>
                             ))}
-                            <td><button onClick={() => setText(rowIndex)}>Set</button></td>
+                            <td><button title='Preview' onClick={() => setText(rowIndex)}>Set</button></td>
                             <td><button title='Set+Play' style={{ backgroundColor: 'darkgreen', color: 'white' }} onClick={() => {
                                 setText(rowIndex);
-                                sendToCasparcg(dataLayer, canvas, currentscreenSize);
+                                setTimeout(() => {
+                                    sendToCasparcg(dataLayer, canvas, currentscreenSize);
+                                }, 1000);
                             }}><FaPlay /></button></td>
                             <td><button onClick={() => {
                                 setText(rowIndex);
@@ -155,6 +232,10 @@ const EditableTable = () => {
                 </tbody>
             </table >
         </div>
+        <div style={{ border: '1px solid red' }}>
+            <GsapPlayer />
+        </div>
+
     </div>);
 };
 
