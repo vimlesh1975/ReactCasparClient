78d82a06d889ebdd05e585f19f948110618c584a
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2024-06-05 12:00:36 +0530

    removed csvreader2

--- client/src/data/Data.jsx
+++ client/src/data/Data.jsx
@@ -1,31 +1,15 @@
 import React from 'react'
 // import CsvReader2 from './CsvReader2'
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+// import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import EditableTable from './EditableTable';
 
 const Data = () => {
-    const onTabChange = () => {
+    // const onTabChange = () => {
 
-    }
+    // }
     return (
         <div>
-            <Tabs
-                // selectedIndex={tabindex}
-                selectedTabClassName="selectedTab"
-                forceRenderTabPanel={true}
-                onSelect={(index, prevIndex) => onTabChange(index, prevIndex)}
-            >
-                <TabList>
-                    <Tab>Data Table</Tab>
-                    {/* <Tab>CSV</Tab> */}
-                </TabList>
-                <TabPanel>
-                    <EditableTable />
-                </TabPanel>
-                {/* <TabPanel>
-                    <CsvReader2 />
-                </TabPanel> */}
-            </Tabs>
+            <EditableTable />
         </div>
     )
 }
