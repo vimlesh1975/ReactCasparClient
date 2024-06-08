4ce4ad378d300542a1ffb66844c6e83954170c12
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2024-06-08 17:03:20 +0530

    add to canvas in imgseq

--- client/src/imagesequence/ImageSequence.jsx
+++ client/src/imagesequence/ImageSequence.jsx
@@ -1,13 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { endpoint, executeScript } from '../common';
+import React, { useState, useRef } from 'react';
+import { shadowOptions, endpoint, executeScript } from '../common';
 import Mixer from '../Mixer';
-// const frameRate = 40;
+import { fabric } from "fabric";
+import { useSelector } from 'react-redux';
+
 const ImageSequence = ({ layer }) => {
+  const canvas = useSelector(state => state.canvasReducer.canvas);
+
   const [base64Images, setBase64Images] = useState([]);
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [imageObjects, setImageObjects] = useState([]);
   const [frameRate, setFrameRate] = useState(60);
   const [preview, setPreview] = useState(false);
   const [imgSequenceLayer, setImgSequenceLayer] = useState(layer);
+
+  const [isScrubbing, setIsScrubbing] = useState(false);
+  const [currentFrame, setCurrentFrame] = useState(0);
+  const animationInterval = useRef(null);
 
   const handleFolderSelect = (event) => {
     const files = Array.from(event.target.files);
@@ -23,21 +31,36 @@ const ImageSequence = ({ layer }) => {
     Promise.all(base64Promises)
       .then((base64Array) => {
         setBase64Images(base64Array);
+        loadPNGSequence(base64Array)
       })
       .catch((error) => {
         console.error('Error reading files:', error);
       });
   };
+  const playAnimation = (startFrame) => {
+    let j = startFrame;
+    animationInterval.current = setInterval(() => {
+      if (!isScrubbing) {
+        showFrame(j);
+        j = (j + 1) % (base64Images.length - 1);
+        setCurrentFrame(j);
+      }
+    }, 1000 / frameRate); // Adjust the frame rate as needed
+  };
+  const loadPNGSequence = async (aa) => {
+    const loadedImages = [];
+    for (let i = 0; i <= aa.length; i++) {
+      await new Promise((resolve) => {
+        fabric.Image.fromURL(aa[i], (image) => {
+          image.set({ opacity: 0 });
+          loadedImages.push(image);
+          resolve(image);
+        });
+      });
+    }
+    setImageObjects(loadedImages);
+  };
 
-  useEffect(() => {
-    if (base64Images.length === 0) return;
-
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % base64Images.length);
-    }, 1000 / frameRate);
-
-    return () => clearInterval(interval);
-  }, [base64Images, frameRate]);
 
   const stop = layerNumber => {
     endpoint(`stop ${window.chNumber}-${layerNumber}`);
@@ -80,7 +103,45 @@ const ImageSequence = ({ layer }) => {
     executeScript(script);
 
   }
+  const addToCanvas = (id = 'id_' + layer) => {
+    const imageGroup = new fabric.Group(imageObjects, {
+      shadow: shadowOptions,
+      id: id,
+      class: "class_" + fabric.Object.__uid,
+      fill: "#ffffff",
+      objectCaching: false,
+      stroke: "#000000",
+      strokeWidth: 0,
+    });
 
+    canvas.add(imageGroup).setActiveObject(imageGroup);;
+    canvas.requestRenderAll();
+  };
+  const showFrame = (frameIndex) => {
+    if (canvas.getObjects().some(image => image.id === `id_${layer}`)) {
+      const group = canvas.getObjects().find(object => object.id === `id_${layer}`)
+      group.getObjects().forEach((image, index) => {
+        image.set({ opacity: index === frameIndex ? 1 : 0 });
+      });
+      canvas.requestRenderAll();
+    }
+  };
+  const handleScrubberChange = (event) => {
+    clearInterval(animationInterval.current);
+    setIsScrubbing(true);
+    const frameIndex = parseInt(event.target.value, 10);
+    showFrame(frameIndex);
+    setCurrentFrame(frameIndex);
+  };
+  const handleScrubberRelease = (event) => {
+    setIsScrubbing(false);
+    const frameIndex = parseInt(event.target.value, 10);
+    showFrame(frameIndex);
+    setCurrentFrame(frameIndex);
+  };
+  const handlePlayButtonClick = () => {
+    playAnimation(currentFrame);
+  };
   return (
     <div>
       <div style={{ display: 'flex' }}>
@@ -103,23 +164,37 @@ const ImageSequence = ({ layer }) => {
             <button onClick={() => setPreview(val => !val)}>{preview ? 'Stop Preview' : 'Preview'}</button>
             <button style={{ backgroundColor: 'darkgreen', color: 'white' }} onClick={() => play(imgSequenceLayer)}>Play to Caspar</button>
             <button style={{ backgroundColor: 'darkred', color: 'white' }} onClick={() => stop(imgSequenceLayer)}>Stop to Caspar</button>
+            <button onClick={() => addToCanvas()}>Add to canvas</button>
+            <div>
+              <input
+                type="range"
+                id="scrubber"
+                min="0"
+                max={base64Images.length - 2}
+                value={currentFrame}
+                step="1"
+                onInput={handleScrubberChange}
+                onChange={handleScrubberRelease}
+              />
+              <button id="playButton" onClick={handlePlayButtonClick}>
+                Play Animation
+              </button>
+            </div>
             <Mixer layer={imgSequenceLayer} setLayer={setImgSequenceLayer} layerVisisble={false} />
-
           </div>
         </div>
         <div>
           <div>
             {preview && base64Images.length > 0 ? (
               <img
-                src={base64Images[currentIndex]}
-                alt={`fgfdg ${currentIndex}`}
+                src={base64Images[currentFrame]}
+                alt={`fgfdg ${currentFrame}`}
                 style={{ width: 200, height: 200, border: '1px solid red' }}
               />
             ) : (
               ''
             )}
           </div>
-
         </div>
       </div>
     </div>
