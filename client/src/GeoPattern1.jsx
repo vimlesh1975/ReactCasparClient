6b191775638cca7ae2abe36fc9640764c36ae511
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2025-02-24 14:13:56 +0530

    Set Pattern from Image

--- client/src/GeoPattern1.jsx
+++ client/src/GeoPattern1.jsx
@@ -41,74 +41,83 @@ const GeoPattern1 = () => {
     const Upload = (e) => {
         if (e.target.files[0]) {
             var reader = new FileReader();
-            reader.onload = event => {
+            reader.onload = () => {
                 canvas.getActiveObjects().forEach(element => {
-                    fabric.util.loadImage(event.target.result, myImg => {
-                        element.set({ fill: new fabric.Pattern({ source: myImg, repeat: 'repeat', offsetX: 0, offsetY: 0 }) })
+                    fabric.util.loadImage(reader.result).then(myImg => {
+                        element.set({
+                            fill: new fabric.Pattern({
+                                source: myImg,
+                                repeat: 'repeat',
+                                offsetX: 0,
+                                offsetY: 0
+                            })
+                        });
                         canvas.requestRenderAll();
-                    })
+                    });
                 });
-            }
+            };
             reader.readAsDataURL(e.target.files[0]);
         }
-    }
-    const changeXoffset = (e) => {
-        canvas.getActiveObjects().forEach(element => {
-            if (element.fill.repeat !== undefined) {
-                element.fill.offsetX = e.target.value
-                canvas.requestRenderAll();
-            }
-        })
-    }
-    const changeYoffset = (e) => {
-        canvas.getActiveObjects().forEach(element => {
-            if (element.fill.repeat !== undefined) {
-                element.fill.offsetY = e.target.value
-                canvas.requestRenderAll();
-            }
-        })
-    }
+    };
+    
 
-    const changerepeat = (e) => {
-        canvas.getActiveObjects().forEach(element => {
-            if (element.fill.repeat !== undefined) {
-                element.fill.repeat = e.target.checked ? 'repeat' : 'no-repeat'
-            }
-        })
-        canvas.requestRenderAll();
-    }
+const changeXoffset = (e) => {
+    canvas.getActiveObjects().forEach(element => {
+        if (element.fill.repeat !== undefined) {
+            element.fill.offsetX = e.target.value
+            canvas.requestRenderAll();
+        }
+    })
+}
+const changeYoffset = (e) => {
+    canvas.getActiveObjects().forEach(element => {
+        if (element.fill.repeat !== undefined) {
+            element.fill.offsetY = e.target.value
+            canvas.requestRenderAll();
+        }
+    })
+}
 
-    return (<div>
+const changerepeat = (e) => {
+    canvas.getActiveObjects().forEach(element => {
+        if (element.fill.repeat !== undefined) {
+            element.fill.repeat = e.target.checked ? 'repeat' : 'no-repeat'
+        }
+    })
+    canvas.requestRenderAll();
+}
 
-        <div
-            style={{
-                width: 200, height: 200,
-                backgroundImage: GeoPattern.generate(string1, {
-                    color: color1,
-                }).toDataUrl()
-            }}
-        >
-        </div>
-        <input
-            type="color"
-            defaultValue={color1}
-            onChange={(e) => setcolor1(e.target.value)}
-        />
-        Change Pattern: <input type={'range'} min={0} max={1920} value={string1} onChange={e => setString1(e.target.value)} />
+return (<div>
 
-        <button onClick={setPattern}>Set Pattern</button>
+    <div
+        style={{
+            width: 200, height: 200,
+            backgroundImage: GeoPattern.generate(string1, {
+                color: color1,
+            }).toDataUrl()
+        }}
+    >
+    </div>
+    <input
+        type="color"
+        defaultValue={color1}
+        onChange={(e) => setcolor1(e.target.value)}
+    />
+    Change Pattern: <input type={'range'} min={0} max={1920} value={string1} onChange={e => setString1(e.target.value)} />
 
-        <div className='drawingToolsRow' >
-            <b> Set Pattern from Image: </b>
-            <input type="file" accept="image/*" onChange={(e) => Upload(e)} />
-            offsetX: <input type={'range'} min={0} max={1920} onChange={e => changeXoffset(e)} />
-            offsetY:<input type={'range'} min={0} max={1080} onChange={e => changeYoffset(e)} />
-            Repeat:<input type={'checkbox'} onChange={e => changerepeat(e)} />
-        </div>
-        <div ref={particlebg} style={{ width: 192, height: 108 }}>
-            {/* <ParticlesBg type="circle" bg={false} /> */}
-        </div>
-    </div >)
+    <button onClick={setPattern}>Set Pattern</button>
+
+    <div className='drawingToolsRow' >
+        <b> Set Pattern from Image: </b>
+        <input type="file" accept="image/*" onChange={(e) => Upload(e)} />
+        offsetX: <input type={'range'} min={0} max={1920} onChange={e => changeXoffset(e)} />
+        offsetY:<input type={'range'} min={0} max={1080} onChange={e => changeYoffset(e)} />
+        Repeat:<input type={'checkbox'} onChange={e => changerepeat(e)} />
+    </div>
+    <div ref={particlebg} style={{ width: 192, height: 108 }}>
+        {/* <ParticlesBg type="circle" bg={false} /> */}
+    </div>
+</div >)
 }
 
 export default GeoPattern1
