4b9957158ebca079c98e9fbce703cd3fe8167ecc
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2025-05-14 13:10:11 +0530

    removed MOS client

--- client/src/MOS/Mos.jsx
+++ client/src/MOS/Mos.jsx
@@ -1,16 +1,9 @@
 import React from 'react'
-import Client from './Client'
-import Server from './Server'
 
 const Mos = () => {
     return (<div>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <div style={{ border: '1px solid red', width: '50%', height: '100vh' }}>
-                <Server />
-            </div>
-            <div style={{ border: '1px solid blue', width: '50%', height: '100vh' }}>
-                <Client />
-            </div>
+            Blank
         </div>
     </div>)
 }
