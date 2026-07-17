ae4e3676bf5009547fabf14f55e4ad758e97accc
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2026-07-17 14:43:25 +0530

    date

--- client/src/common.js
+++ client/src/common.js
@@ -4,7 +4,7 @@ import * as fabric from "fabric";
 import _ from "lodash";
 import * as d from "@theatre/dataverse";
 
-export const buildDate = "240226_1";
+export const buildDate = "17072026_1";
 
 
 
